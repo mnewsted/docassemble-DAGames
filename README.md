6192079fa63b38c71dfa0446f3253faf3f040370
Matt Newsted <mnewsted@illinoislegalaid.org>
2024-05-11 19:22:07 -0500

    initial-commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# docassemble.DAGames
+
+A docassemble extension.
+
+## Author
+
+Matt Newsted, mnewsted@illinoislegalaid.org
+
---
+++ docassemble/DAGames/rooms.py
@@ -0,0 +1,41 @@
+from docassemble.base.util import (
+    DAList
+    )
+
+
+class Room(DAList):
+  
+  def init(self, *pargs, **kwargs):
+    super().init(*pargs, **kwargs)
+    self.object_type = DAList
+
+  def is_valid_exit(self, location, direction):
+    if direction in self[location].exits:
+      return True
+    else:
+      return False   
+
+  def visible_exits(self, location):
+    exits = []
+    for dir in self[location].exits:
+      exits.append(full_direction(dir))
+    return exits
+                   
+def short_direction(dir):
+  if len(dir) == 1:
+    return dir
+  if len(dir) == 2:
+    if dir == 'up':
+      return dir[0]
+    else:
+      return dir
+  if dir in [ 'north', 'south', 'east', 'west', 'up', 'down']:
+    return dir[0]
+  else:
+    return dir[0] + dir[5]
+
+  
+def full_direction(dir):
+  direction_pairs = { 'n':'north', 's':'south', 'e':'east', 'w':'west', 'u':'up', 'd':'down', 'nw':'northwest', 'ne':'northeast', 'sw':'southwest', 'se':'southeast' }
+  short_dir = short_direction(dir)
+  return direction_pairs[short_dir]
