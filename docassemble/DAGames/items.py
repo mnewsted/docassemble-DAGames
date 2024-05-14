99bb2863e23c2c5e7a37b6865c6f81bec303a2b6
Matt Newsted <mnewsted@illinoislegalaid.org>
2024-05-13 19:38:33 -0500

    adding-items-functions

--- docassemble/DAGames/items.py
+++ docassemble/DAGames/items.py
@@ -16,4 +16,16 @@ class ItemList(DAList):
       if item.room == location:
         visible_things.append(item.name.text)
     return visible_things
+  
+  def inventory(self):
+    held_things = []
+    for item in self:
+      if item.room == 999:
+        held_things.append(item.name.text)
+    return held_things
+  
+  def usable_items(self, location):
+    usable_things = []
+    usable_things = self.inventory() + self.visible_items(location)
+    return usable_things
     
