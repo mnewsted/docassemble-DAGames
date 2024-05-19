0005491c23309bd2cf35a8c04a72b5f3bec0a262
Matt Newsted <mnewsted@illinoislegalaid.org>
2024-05-19 17:57:34 -0500

    events-in-progress

--- docassemble/DAGames/events.py
+++ docassemble/DAGames/events.py
@@ -10,19 +10,11 @@ class EventList(DAList):
     super().init(*pargs, **kwargs)
     self.object_type = Thing
 
-def event_check(self, location, target):
-  for event in events:
-    if event.exists(location, target):
-      if event.done == False:
-        return event.name.text, event.effect_index
-      else:
-        return event.aldready_done_text, 999
-    else:
-  return "pass", -1
-
-def exists(self, location, target):
-  for event in events:
-    if event.location == location and event.trigger_item == target:
-      return True
-    else:
-      return False
+  def event_check(self, location, target):
+    for event in self:
+      if (event.location == location or event.location == 999) and event.trigger_item == target:
+        if event.done == False:
+          return event.name.text, event.effect_index
+        else:
+          return event.already_done_text, 999
+    return "pass", -1
